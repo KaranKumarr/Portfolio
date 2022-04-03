801418f19ad992d7cd15c40ef1b4c828f3710905
Karan Kumar <karankumar0334@gmail.com>
2022-04-04 02:50:17 +0500

    Skills Icons Grid Improv

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,10 +9,10 @@ import tailwindcss from './resources/imgs/TailwindCss.svg';
 
 export default function Skills() {
     return (
-        <div className='border-blueBell-400 border-4 rounded-3xl  my-10 py-10 lg:mx-30 lg:px-10 md:mx-18 md:px-10 sm:mx-10 sm:px-6 mx-4 px-4'>
+        <div className='border-blueBell-400 md:border-4 md:border-r-8 md:border-b-8 border-2 border-r-4 border-b-4 rounded-3xl  my-10 py-10 lg:mx-30 lg:px-10 md:mx-18 md:px-10 sm:mx-10 sm:px-6 mx-4 px-4'>
             <h1 className='text-blueBell-500 font-bold sec-heading'>Skills</h1>
             <p className='text-blueBell-500 subtitle tracking-wider text-center p-2'>I'm fascinated by cutting-edge technologies. Here are some skills and tools that I am comfortable with or currently learning.</p>
-            <div className='grid-layout p-12'>
+            <div className='grid grid-layout grid-cols-2 sm:p-12 py-8 px-6'>
                 <SkillsIcon link={'#'} img={react} />
                 <SkillsIcon link={'#'} img={nodejs} />
                 <SkillsIcon link={'#'} img={mysql} />
--- src/components/SkillsIcon.jsx
+++ src/components/SkillsIcon.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function SkillsIcon({ img, link }) {
     return (
         <a href={link} className='bg-blueBell-50 bg-opacity-50 opacity-90 hover:opacity-100 transition-all ease-in rounded-md hover:rounded-2xl p-2 flex justify-center items-center'>
-            <img src={img} alt="Skill Icon" className='w-28 h-28' />
+            <img src={img} alt="Skill Icon" className='md:w-28 md:h-28 sm:w-24 sm:h-24 w-20 h-20' />
         </a>
     );
 }
