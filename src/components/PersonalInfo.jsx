adc0293239d146b8d2f928d203b227ff22614dc1
Karan Kumar <59010694+KaranKumarr@users.noreply.github.com>
2023-03-06 20:55:17 +0500

    Update PersonalInfo.jsx

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -17,7 +17,7 @@ export default function PersonalInfo() {
             <div className='bg-blueBell-600 text-white text-center lg:p-40 md:p-28 sm:p-16 p-8'>
                 <h1 className='font-bold main-heading sm:my-10 my-4'>HI, I AM KARAN KUMAR.</h1>
                 <p className='subtitle'>
-                    I am a rising junior at Szabist University majoring in CS. I also do freelance work on Fiverr, mainly I work on web development. I have worked on multiple languages and technologies but go-to programming language has always been Javascript. I am currently looking for internship OR full-time opportunity to learn new skills and sharpen already existing ones.
+                    I am a rising senior at Szabist University Karachi majoring in CS. I also do freelance work on Fiverr, mainly I work on web development. I have worked on multiple languages and technologies but go-to programming language has always been Javascript. I am currently looking for internship OR full-time opportunity to learn new skills and sharpen already existing ones.
                 </p>
                 <div className='flex justify-center items-center'>
                     <Icons />
